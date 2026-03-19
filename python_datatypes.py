d39717417b34a59d914bcdb9bbc4544281f53432
baragadegeeta18 <baragadegeeta795@gmail.com>
2026-03-19 15:18:23 +0530

    Add python_datatypes.py to demonstrate data types
    This file demonstrates various Python data types including integer, float, string, boolean, list, tuple, dictionary, and set.

---
+++ python_datatypes.py
@@ -0,0 +1,43 @@
+# Integer
+a = 30
+print(a)
+print(type(a))
+
+# Float
+marks = 88.60
+print(marks)
+print(type(marks))
+
+# String
+name = "Geeta Baragade"
+print(name)
+print(type(name))
+
+# Boolean
+b = True
+print(b)
+print(type(b))
+
+# List
+x = [5, 15, 25, 35, 45]
+print(x)
+print(type(x))
+
+# Tuple
+numbers = (2, 4, 6, 8)
+print(numbers)
+print(type(numbers))
+
+# Dictionary
+xyz = {
+"Name": "Geeta Baragade",
+"Age": 19,
+"Course": "Mechanical Engineering"
+}
+print(xyz)
+print(type(xyz))
+
+# Set
+colors = {"Pink", "Orange", "Purple"}
+print(colors)
+print(type(colors))
